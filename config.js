bc03b865c728ba2f76277732b2ac01ed52a8c362
Heberty <hebertyalves15@gmail.com>
2026-06-27 16:23:42 -0300

    Atualiza o config.js com a URL do supabase

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   appName: "LocaControl",
   storageMode: "supabase",
-  supabaseUrl: "https://yrhedtwlesqqrjuuoqkj.supabase.co",
+  supabaseUrl: "https://twymntstwnuqppouvvsr.supabase.co/rest/v1/",
   supabaseAnonKey: "sb_publishable_HYKM77JgOc4KKFGse5SJkw_afWRIYuI",
 };
